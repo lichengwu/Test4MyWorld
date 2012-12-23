f8fbd823e2c41996bac203ee9c25de5e8226d849
lichengwu <lichengwu@meituan.com>
2012-12-23 14:53:08 +0800

    apache poi test

--- src/main/java/oliver/test/apache/poi/MergeSheets.java
+++ src/main/java/oliver/test/apache/poi/MergeSheets.java
@@ -31,26 +31,19 @@ public class MergeSheets {
                     "E:\\Dropbox\\document\\meituan\\url_request.xls");
             POIFSFileSystem fs = new POIFSFileSystem(fis);
             HSSFWorkbook wb = new HSSFWorkbook(fs);
-            for (String iUrl : urlList) {
-                System.out.print(iUrl + ",");
                 for (int i = 0; i < wb.getNumberOfSheets(); i++) {
                     int hit = 0;
                     int total = 0;
                     HSSFSheet sheet = wb.getSheetAt(i);
                     for (int j = 0; j <= sheet.getLastRowNum(); j++) {
                         String url = sheet.getRow(j).getCell(0).getStringCellValue();
-                        if (url.startsWith(iUrl)) {
                             // System.out.println(i+":"+j);
                             hit += new Double(sheet.getRow(j).getCell(2).getNumericCellValue()).intValue();
                             total += new Double(sheet.getRow(j).getCell(2).getNumericCellValue())
                                     .intValue()
                                     * new Double(sheet.getRow(j).getCell(3).getNumericCellValue())
                                     .intValue();
-                        }
                     }
-                    System.out.print(hit + "(" + total / Math.max(hit, 1)+")" + ",");
-                }
-                System.out.println();
             }
         } finally {
             if (fis != null) {
