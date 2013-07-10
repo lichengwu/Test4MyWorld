3b9c61fd3a7defc4f4d915ae672652fba3ee6caf
lichengwu <ol_l@msn.cn>
2013-07-10 19:14:24 +0800

    test

---
+++ src/main/java/oliver/test/Main.java
@@ -0,0 +1,20 @@
+package oliver.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 临时
+ *
+ * @author 佐井
+ * @version 1.0
+ * @created 2013-07-04 1:59 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("1");
+
+        System.out.println(set.add("1"));
+    }
+}
